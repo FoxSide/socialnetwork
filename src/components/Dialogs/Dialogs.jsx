48d61f8718655f9552f83d69507cf0e21dcc1ee8
FoxSide <foxside8452@gmail.com>
2021-09-01 19:31:51 +0300

    21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          FoxSide
+        </div>
+        <div className={s.dialog}>
+          Nekrsova
+        </div>
+        <div className={s.dialog}>
+          Ned
+        </div>
+        <div className={s.dialog}>
+          Varova
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>
+          Hi
+        </div>
+        <div className={s.message}>
+          Hello
+        </div>
+        <div className={s.message}>
+          Privet
+        </div>
+      </div>
     </div>
   )
 }
