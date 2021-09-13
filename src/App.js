0217d5a858cc27169350767bc5dfb148a2c74f88
FoxSide <foxside8452@gmail.com>
2021-09-13 18:09:14 +0300

    28

--- src/App.js
+++ src/App.js
@@ -10,34 +10,14 @@ import Profile from './components/Profile/Profile';
 import Settings from './components/Settings/Settings';
 
 const App = (props) => {
-
-  let posts = [
-    { id: 1, message: 'Hi, how are you?', likesCount: 10 },
-    { id: 2, message: 'It\'s my first post', likesCount: 15 }
-  ];
-
-  let dialogs = [
-    { id: 1, name: 'FoxSide' },
-    { id: 2, name: 'Nekrsova' },
-    { id: 3, name: 'Ned' },
-    { id: 4, name: 'Varova' }
-  ];
-
-  let messages = [
-    { id: 1, message: 'Hi' },
-    { id: 2, message: 'How are you' },
-    { id: 3, message: '?' },
-    { id: 4, message: ';)' }
-  ];
-
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
         <Header />
         <Navbar />
         <div class='app-wrapper-content'>
-          <Route path='/dialogs' render={ () => <Dialogs dialogs={dialogs} messages={messages}/> }/>
-          <Route path='/profile' render={ () => <Profile posts={posts} /> }/>
+          <Route path='/dialogs' render={ () => <Dialogs dialogs={props.dialogs} messages={props.messages}/> }/>
+          <Route path='/profile' render={ () => <Profile posts={props.posts} /> }/>
           <Route path='/news' render={ () => <News /> }/>
           <Route path='/music' render={ () => <Music /> }/>
           <Route path='/settings' render={ () => <Settings /> }/>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let posts = [
+  { id: 1, message: 'Hi, how are you?', likesCount: 10 },
+  { id: 2, message: 'It\'s my first post', likesCount: 15 }
+];
+
+let dialogs = [
+  { id: 1, name: 'FoxSide' },
+  { id: 2, name: 'Nekrsova' },
+  { id: 3, name: 'Ned' },
+  { id: 4, name: 'Varova' }
+];
+
+let messages = [
+  { id: 1, message: 'Hi' },
+  { id: 2, message: 'How are you' },
+  { id: 3, message: '?' },
+  { id: 4, message: ';)' }
+];
+
+ReactDOM.render(<React.StrictMode><App posts={posts} dialogs={dialogs} messages={messages}/></React.StrictMode>, document.getElementById('root'));
 
 
 reportWebVitals();
