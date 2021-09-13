8283b9c7d35d6dae4d84fee0892b7c9639b8e081
FoxSide <foxside8452@gmail.com>
2021-09-13 18:59:11 +0300

    29

--- src/index.js
+++ src/index.js
@@ -3,27 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
-let posts = [
-  { id: 1, message: 'Hi, how are you?', likesCount: 10 },
-  { id: 2, message: 'It\'s my first post', likesCount: 15 }
-];
-
-let dialogs = [
-  { id: 1, name: 'FoxSide' },
-  { id: 2, name: 'Nekrsova' },
-  { id: 3, name: 'Ned' },
-  { id: 4, name: 'Varova' }
-];
-
-let messages = [
-  { id: 1, message: 'Hi' },
-  { id: 2, message: 'How are you' },
-  { id: 3, message: '?' },
-  { id: 4, message: ';)' }
-];
-
-ReactDOM.render(<React.StrictMode><App posts={posts} dialogs={dialogs} messages={messages}/></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><App state={state}/></React.StrictMode>, document.getElementById('root'));
 
 
 reportWebVitals();
---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, message: 'Hi, how are you?', likesCount: 10 },
+      { id: 2, message: 'It\'s my first post', likesCount: 15 }
+    ],
+  },
+  dialogsPage: {
+    messages: [
+      { id: 1, message: 'Hi' },
+      { id: 2, message: 'How are you' },
+      { id: 3, message: '?' },
+      { id: 4, message: ';)' }
+    ],
+    dialogs: [
+      { id: 1, name: 'FoxSide' },
+      { id: 2, name: 'Nekrsova' },
+      { id: 3, name: 'Ned' },
+      { id: 4, name: 'Varova' }
+    ],
+  },
+}
+
+export default state
